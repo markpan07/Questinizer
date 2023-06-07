6bad92e2652853ed1ffa1a69255d08b4ae9a5fce
МАРК <markpan07@mail.ru>
2023-06-07 11:33:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/questinizer/QuestinizerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.questinizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestinizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestinizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/questinizer/QuestinizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.questinizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestinizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
